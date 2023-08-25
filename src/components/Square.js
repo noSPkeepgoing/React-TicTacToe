54b2fc49a16ab3d80ee8c65ea7f847eaa79383da
sunpaaaa <sunpaaaa@gmail.com>
2023-08-25 12:09:35 +0900

    Refactor using functional component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 import './Square.css';
 
-export default class Square extends Component {
-  render() {
-    return (
-      <button
-        onClick={() => {
-          this.props.onClick();
-        }}
-        className='square'
-      >
-        {this.props.value}
-      </button>
-    );
-  }
-}
+const Square = ({ onClick, value }) => {
+  return (
+    <button onClick={onClick} className='square'>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
